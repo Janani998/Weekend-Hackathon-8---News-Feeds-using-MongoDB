3b87409bf247f627b78d6ac9402dcc21343e18b9
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-11 23:26:49 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,48 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const newsArticleModel = require('./connector')
 
+app.get('/newFeeds',paginatedResults(newsArticleModel),(req,res)=>{
+    res.send(res.paginatedElements)
+})
 
+function paginatedResults(model){ 
+    return async (req,res,next) =>{
+        const {limit,offset} = req.query;
+        const options = {
+            limit : parseInt(limit,10) || 10,
+            offset : parseInt(offset,10) || 0
+        }
+        
+        const startIndex = (options.offset -1) * options.limit
+        const endIndex = options.offset * options.limit
+    
+        const results = {}
+    
+        if(endIndex < await model.countDocuments().exec()){
+            results.next = {
+                offset : options.offset + 1,
+                limit : options.limit
+            }
+        }
+        
+        if(startIndex > 0){
+            results.previous = {
+                offset : options.offset - 1,
+                limit : options.limit
+            }
+        }
+        
+        try{
+            results.results = await model.find().limit(options.limit).skip(startIndex).exec()
+            res.paginatedElements = results
+            next()
+        } catch (e) {
+            res.status(500).send({message : e.message})
+        }
+    }
+}
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
