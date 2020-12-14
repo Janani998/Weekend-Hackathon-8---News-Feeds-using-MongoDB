e7e7d92ed7d3ee7ed5c07d24d1c4f5d44b209190
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-15 00:30:42 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -10,50 +10,27 @@ app.use(express.json());
 
 const { newsArticleModel } = require("./connector");
 
-app.get("/newFeeds", paginatedResults(newsArticleModel), (req, res) => {
-  res.status(200).send(res.paginatedElements);
+app.get("/newFeeds", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  const offset = parseInt(req.query.offset);
+  const options = {};
+  options.skip = limit * (offset - 1);
+  options.limit = limit;
+  newsArticleModel.count({}, function (err, totalCount) {
+    if (err) {
+      res.status(500).send({ message: err.message });
+      return;
+    }
+    newsArticleModel.find({}, {}, options, function (err, result) {
+      if (err) {
+        res.status(500).send({ message: err.message });
+        return;
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
 });
-
-function paginatedResults(model) {
-  return async (req, res, next) => {
-    const { limit, offset } = req.query;
-    const options = {
-      limit: parseInt(limit, 10) || 10,
-      offset: parseInt(offset, 10) || 0
-    };
-
-    const startIndex = (options.offset - 1) * options.limit;
-    const endIndex = options.offset * options.limit;
-
-    const results = {};
-
-    if (endIndex < (await model.countDocuments().exec())) {
-      results.next = {
-        offset: options.offset + 1,
-        limit: options.limit
-      };
-    }
-
-    if (startIndex > 0) {
-      results.previous = {
-        offset: options.offset - 1,
-        limit: options.limit
-      };
-    }
-
-    try {
-      results.results = await model
-        .find()
-        .limit(options.limit)
-        .skip(startIndex)
-        .exec();
-      res.paginatedElements = results;
-      next();
-    } catch (e) {
-      res.status(500).send({ message: e.message });
-    }
-  };
-}
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
