83dfc2377249b77568e52196b9c6b0ced819f49f
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-15 01:02:37 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -11,19 +11,12 @@ app.use(express.json());
 const { newsArticleModel } = require("./connector");
 
 app.get("/newFeeds", (req, res) => {
-  let limit = parseInt(req.query.limit);
-  let offset = parseInt(req.query.offset);
-  if (!limit) {
-    limit = onePageArticleCount;
-  }
-  if (!offset) {
-    offset = 0;
-  }
-
+  const limit = parseInt(req.query.limit);
+  const offset = parseInt(req.query.offset);
   const options = {};
   options.skip = limit * (offset - 1);
   options.limit = limit;
-  newsArticleModel.count({}, function (err) {
+  newsArticleModel.count({}, function (err, totalCount) {
     if (err) {
       res.status(500).send({ message: err.message });
       return;
@@ -38,7 +31,6 @@ app.get("/newFeeds", (req, res) => {
     });
   });
 });
-
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
