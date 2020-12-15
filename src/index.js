76b905f626438331abb899a68583d823f969c6fb
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-15 12:30:15 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -13,33 +13,37 @@ const { newsArticleModel } = require("./connector");
 app.get("/newFeeds", (req, res) => {
   let limitReceived = req.query.limit;
   let offsetReceived = req.query.offset;
-  let limit = parseInt(limitReceived,10);
-  let offset = parseInt(offsetReceived,10);
+  let limit = parseInt(limitReceived, 10);
+  let offset = parseInt(offsetReceived, 10);
   if (!limitReceived) {
     limit = onePageArticleCount;
   }
   if (!offsetReceived) {
     offset = 0;
   }
-  if(isNaN(offset)){
-    offset = 0
+  if (isNaN(offset)) {
+    offset = 0;
   }
-  if(isNaN(limit)){
+  if (isNaN(limit)) {
     limit = onePageArticleCount;
-    if(!offsetReceived){
+    if (!offsetReceived) {
       offset = 0;
     }
   }
 
-    newsArticleModel.find().then(result =>{
-      let startIndex =0;
+  newsArticleModel
+    .find()
+    .then((result) => {
+      let startIndex = 0;
       startIndex += offset;
       let returnedArray = [];
-      for(let i = startIndex; i<startIndex+limit;i++){
-        returnedArray.push(result[i])
+      for (let i = startIndex; i < startIndex + limit; i++) {
+        returnedArray.push(result[i]);
       }
       res.status(200).send(returnedArray);
-    }).catch(error => error.message);
+    })
+    .catch((error) => error.message);
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
