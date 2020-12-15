3d11239bac535a3f080212dffe3ffc6a3475fa05
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-15 12:01:41 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -33,7 +33,60 @@ app.get("/newFeeds", (req, res) => {
   options.limit = limit;
   newsArticleModel.countDocuments({}, function (err) {
     if (err) {
-      res.status(500).send({ message: err.message });
+      res.status(500).sconst express = require("express");
+      const app = express();
+      const port = 8080;
+      
+      const onePageArticleCount = 10;
+      
+      // Parse JSON bodies (as sent by API clients)
+      app.use(express.urlencoded({ extended: false }));
+      app.use(express.json());
+      
+      const { newsArticleModel } = require("./connector");
+      
+      app.get("/newFeeds", (req, res) => {
+        let limitReceived = req.query.limit;
+        let offsetReceived = req.query.offset;
+        let limit = parseInt(limitReceived,10);
+        let offset = parseInt(offsetReceived,10);
+        if (!limitReceived) {
+          limit = onePageArticleCount;
+        }
+        if (!offsetReceived) {
+          offset = 1;
+        }
+        if(isNaN(offset)){
+          offset = 1
+        }
+        if(isNaN(limit)){
+          limit = onePageArticleCount;
+          if(!offsetReceived){
+            offset = 1;
+          }
+        }
+        const options = {};
+        options.skip = offset - 1;
+        options.limit = limit;
+        newsArticleModel.countDocuments({}, function (err) {
+          if (err) {
+            res.status(500).send({ message: err.message });
+            return;
+          }
+          newsArticleModel.find({}, {}, options, function (err, result) {
+            if (err) {
+              res.status(500).send({ message: err.message });
+              return;
+            } else {
+              res.status(200).send(result);
+            }
+          });
+        });
+      });
+      app.listen(port, () => console.log(`App listening on port ${port}!`));
+      
+      module.exports = app;
+      end({ message: err.message });
       return;
     }
     newsArticleModel.find({}, {}, options, function (err, result) {
