f8762eb7abc36c6c6a43e2ea64a417fe15642e21
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-15 12:20:28 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -13,41 +13,32 @@ const { newsArticleModel } = require("./connector");
 app.get("/newFeeds", (req, res) => {
   let limitReceived = req.query.limit;
   let offsetReceived = req.query.offset;
-  let limit = parseInt(limitReceived, 10);
-  let offset = parseInt(offsetReceived, 10);
+  let limit = parseInt(limitReceived,10);
+  let offset = parseInt(offsetReceived,10);
   if (!limitReceived) {
     limit = onePageArticleCount;
   }
   if (!offsetReceived) {
-    offset = 1;
+    offset = 0;
   }
-  if (isNaN(offset)) {
-    offset = 1;
+  if(isNaN(offset)){
+    offset = 0
   }
-  if (isNaN(limit)) {
+  if(isNaN(limit)){
     limit = onePageArticleCount;
-    if (!offsetReceived) {
-      offset = 1;
+    if(!offsetReceived){
+      offset = 0;
     }
-  }
-  const options = {};
-  options.skip = offset - 1;
-  options.limit = limit;
-  newsArticleModel.countDocuments({}, function (err) {
-    if (err) {
-      res.status(500).send({ message: err.message });
-      return;
-    }
-    newsArticleModel.find({}, {}, options, function (err, result) {
-      if (err) {
-        res.status(500).send({ message: err.message });
-        return;
-      } else {
-        res.status(200).send(result);
+
+    newsArticleModel.find().then(result =>{
+      let startIndex =0;
+      startIndex += offset;
+      let returnedArray = [];
+      for(let i = startIndex; i<startIndex+limit;i++){
+        returnedArray.push(result[i])
       }
-    });
-  });
-});
+      res.status(200).send(returnedArray);
+    }).catch(error => error.message);
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
